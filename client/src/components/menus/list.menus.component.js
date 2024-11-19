65b03b4280c6d75e21781a74a88b1ffc4641c2d1
Léo Baccialone <baccialone.leo@gmail.com>
2024-11-19 13:06:55 +0100

    wip menus

--- client/src/components/menus/list.menus.component.js
+++ client/src/components/menus/list.menus.component.js
@@ -17,51 +17,63 @@ export default function ListMenusComponent() {
   }
 
   return (
-    <section className="py-24">
-      <div className="flex flex-col gap-24 max-w-[80%] mx-auto">
+    <section className="">
+      <div
+        className="flex flex-col gap-48 max-w-[80%] mx-auto"
+        style={{
+          fontFamily: "'Abel', sans-serif",
+        }}
+      >
         {restaurantContext?.restaurantData?.menus
           .filter((menu) => menu.visible)
           .map((menu, i) => {
             return (
-              <div key={i} className="menu">
-                <h2>{menu?.name}</h2>
-                <p>{menu?.description}</p>
-                <p>
-                  {menu?.price} {menu?.price && "€"}
-                </p>
+              <div key={i} className="flex gap-12 items-center sticky top-[62px] bg-extraWhite" style={{height : 'calc(100vh - 158px)'}}>
+                <div className="w-1/2 flex flex-col">
+                  <h2
+                    className="text-5xl"
+                    style={{ fontFamily: "'Dancing Script', cursive" }}
+                  >
+                    {menu?.name}
+                  </h2>
+
+                  <p className="mb-8 pl-2 text-lg">
+                    {menu?.price} {menu?.price && "€"}
+                  </p>
+
+                  <div className="relative ml-[40px]">
+                    <p className="pl-[40px] text-lg">{menu?.description}</p>
+                    
+                    <hr className="absolute w-[1px] h-full bg-brown opacity-20 top-0 " />
+                  </div>
+                </div>
 
                 {menu.dishes.length > 0 ? (
-                  <div className="menu-dishes">
+                  <div className="w-1/2 text-center flex flex-col gap-16">
                     {Object.entries(groupDishesByCategory(menu.dishes)).map(
-                      ([category, dishes], index) => (
-                        <div key={index} className="category">
-                          <ul>
-                            {dishes.map((dish) => (
-                              <li key={dish._id}>
-                                <span>{dish.name}</span>
-                              </li>
-                            ))}
-                          </ul>
+                      ([_, dishes], index) => (
+                        <div key={index}>
+                          {dishes.map((dish, dishIndex) => (
+                            <div key={dish._id} className="text-lg">
+                              <p>{dish.name}</p>
+                              {dishIndex < dishes.length - 1 && <p>ou</p>}
+                            </div>
+                          ))}
                         </div>
                       )
                     )}
                   </div>
                 ) : (
-                  <div className="menu-combinations">
-                    {menu.combinations.length > 0 ? (
-                      menu.combinations.map((combo, index) => (
-                        <div key={index} className="combination">
-                          <ul>
-                            {combo.categories.map((category, catIndex) => (
-                              <li key={catIndex}>{category}</li>
-                            ))}
-                          </ul>
-                          <p>Prix: {combo.price} €</p>
-                        </div>
-                      ))
-                    ) : (
-                      <p>Ce menu n'a pas de plats ou de combinaisons.</p>
-                    )}
+                  <div className="w-1/2 text-center flex flex-col gap-6">
+                    {menu.combinations.map((combo, index) => (
+                      <div key={index} className="flex flex-col gap-2">
+                        <p className="flex items-center justify-center">
+                          {combo.categories.join(" - ")}
+                        </p>
+
+                        <p>{combo.price} €</p>
+                      </div>
+                    ))}
                   </div>
                 )}
               </div>
