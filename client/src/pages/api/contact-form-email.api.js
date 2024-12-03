8d997c4a88c8e3df65fc39bd8ec0050d1e2bcdc7
Gusto Manager <gusto.workspace@gmail.com>
2024-12-03 16:41:53 +0100

    test mail

--- client/src/pages/api/contact-form-email.api.js
+++ client/src/pages/api/contact-form-email.api.js
@@ -1,94 +1,69 @@
 import SibApiV3Sdk from "sib-api-v3-sdk";
 
-function instantiateClient() {
-  try {
-    const defaultClient = SibApiV3Sdk.ApiClient.instance;
-    const apiKey = defaultClient.authentications["api-key"];
+async function sendTransactionalEmail(params) {
+  const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
-    apiKey.apiKey = process.env.BREVO_API_KEY;
-    return defaultClient;
-  } catch (err) {
-    console.error("Erreur lors de l'instanciation du client:", err);
-    throw new Error(err);
-  }
-}
-
-function sendTransactionalEmail(params) {
-  try {
-    instantiateClient();
-
-    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-
-    let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
-
-    if (params.contact) {
-      sendSmtpEmail.htmlContent = `<html>
-                                      <body>
-                                        <p><strong>Nom:</strong> ${params.name}</p>
-                                        <p><strong>Email:</strong> ${params.email}</p>
-                                        <p><strong>Téléphone:</strong> ${params.phone}</p>
-                                        <p><strong>Message:</strong> ${params.message}</p>
-                                      </body>
-                                     </html>`;
-    }
-
-    sendSmtpEmail.sender = {
+  // Création de l'objet pour l'envoi de l'email
+  const sendSmtpEmail = {
+    sender: {
       email: "no-reply@lacoquille-concarneau.fr",
       name: "Formulaire Contact",
-    };
-
-    sendSmtpEmail.replyTo = {
+    },
+    to: params.to,
+    subject: params.subject,
+    htmlContent: `
+      <html>
+        <body>
+          <p><strong>Nom:</strong> ${params.name}</p>
+          <p><strong>Email:</strong> ${params.email}</p>
+          <p><strong>Téléphone:</strong> ${params.phone}</p>
+          <p><strong>Message:</strong> ${params.message}</p>
+        </body>
+      </html>`,
+    replyTo: {
       email: params.email,
-      name: params.email,
-    };
+      name: params.name,
+    },
+  };
 
-    sendSmtpEmail.to = params.to;
-    sendSmtpEmail.subject = params.subject;
-
-    apiInstance
-      .sendTransacEmail(sendSmtpEmail)
-      .then((data) => {
-        console.log("Email envoyé avec succès :", data);
-      })
-      .catch((error) => {
-        console.error(
-          "Erreur lors de l'envoi de l'email:",
-          error.response ? error.response.body : error
-        );
-      });
-  } catch (err) {
-    console.error("Erreur dans sendTransactionalEmail:", err);
-    throw new Error(err);
+  try {
+    // Envoi de l'email via l'API
+    const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
+    console.log("Email envoyé avec succès :", data);
+    return data;
+  } catch (error) {
+    console.error(
+      "Erreur lors de l'envoi de l'email:",
+      error.response ? error.response.body : error
+    );
+    throw error;
   }
 }
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === "POST") {
+    const data = req.body;
+
+    // Préparation des paramètres pour l'envoi
+    const params = {
+      to: [{ email: "contact@gusto-manager.com", name: "Marie" }],
+      subject: "Nouveau message via le formulaire de contact",
+      name: data.name,
+      email: data.email,
+      phone: data.phone,
+      message: data.message,
+    };
+
     try {
-      const data = req.body;
-
-      const paramsEmail = {
-        to: [{ email: "restaurant-la-coquille@orange.fr", name: "Marie" }],
-        contact: true,
-        subject: "Nouveau message via le formulaire de contact",
-        name: data.name,
-        email: data.email,
-        phone: data.phone,
-        message: data.message,
-      };
-
-      sendTransactionalEmail(paramsEmail);
-
-      return res
-        .status(200)
-        .json({ status: 200, message: "Email envoyé avec succès" });
-    } catch (err) {
-      console.error("Erreur lors de la gestion de la requête POST:", err);
+      await sendTransactionalEmail(params);
+      return res.status(200).json({ message: "Email envoyé avec succès" });
+    } catch (error) {
+      console.error("Erreur d'envoi :", error);
       return res
         .status(500)
-        .json({ status: 500, message: "Erreur lors de l'envoi de l'email" });
+        .json({ message: "Erreur lors de l'envoi de l'email" });
     }
   } else {
-    return res.status(405).json({ message: "Méthode non autorisée" });
+    res.status(405).json({ message: "Méthode non autorisée" });
   }
 }
